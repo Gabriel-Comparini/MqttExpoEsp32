c748b4a759e0948b918de5aa10cca108e99d7844
unknown <gc77777777777777@gmail.com>
2026-05-28 15:25:29 -0300

    feat: add asyncStorage to the app.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { env } from 'expo-env';
 import { StyleSheet, View, Text } from 'react-native';
 import MQTTService from './src/services/mqttService';
 import StatusModal from './src/components/StatusModal';
 import LightControl from './src/components/LightControl';
 import Gauges from './src/components/Gauges';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const mqtt = new MQTTService();
 
@@ -16,28 +16,24 @@ export default function App() {
     const [hum, setHum] = useState(0);
 
     const mqttConfig = {
-        host: env.MQTT_HOST,
-        port: parseInt(env.MQTT_PORT),
-        path: env.MQTT_PATH,
-        user: env.MQTT_USER,
-        pass: env.MQTT_PASS,
+        host: process.env.EXPO_PUBLIC_MQTT_HOST,
+        port: parseInt(process.env.EXPO_PUBLIC_MQTT_PORT),
+        path: process.env.EXPO_PUBLIC_MQTT_PATH,
+        user: process.env.EXPO_PUBLIC_MQTT_USER,
+        pass: process.env.EXPO_PUBLIC_MQTT_PASS,
         clientId: 'RN_App_' + Math.random(),
     };
-
-    useEffect(() => {
-        startConnection();
-    }, []);
 
     const startConnection = () => {
         setShowError(false);
         mqtt.connect(mqttConfig, (topic, message) => {
             if (topic === 'casa/temp') setTemp(parseFloat(message));
-            if (topic === 'casa/umid') setHum(parseFloat(message));
+            if (topic === 'casa/hum') setHum(parseFloat(message));
             if (topic === 'casa/luz') setIsLightOn(message === "1");
         }, () => {
             setIsConnected(true);
             mqtt.subscribe('casa/temp');
-            mqtt.subscribe('casa/umid');
+            mqtt.subscribe('casa/hum');
             mqtt.subscribe('casa/luz');
         }, (err) => {
             setIsConnected(false);
@@ -50,6 +46,32 @@ export default function App() {
         const newState = isLightOn ? "0" : "1";
         mqtt.publish('casa/luz', newState);
     };
+
+    useEffect(() => {
+        async function init(){
+            const asyncHum = await AsyncStorage.getItem("hum");
+            const asyncTemp = await AsyncStorage.getItem("temp");
+            const asyncLight = await AsyncStorage.getItem("light");
+            
+            startConnection();
+
+            if (asyncHum !== null) setHum(parseFloat(asyncHum));
+            if (asyncTemp !== null) setTemp(parseFloat(asyncTemp));
+            if (asyncLight !== null) setIsLightOn(asyncLight === "true");
+        }
+
+        init();
+    }, []);
+
+    useEffect(() => {
+        async function init() {
+            await AsyncStorage.setItem("hum", String(hum));
+            await AsyncStorage.setItem("temp", String(temp));
+            await AsyncStorage.setItem("light", String(isLightOn));
+        }
+
+        init();
+    }, [hum, temp, isLightOn]);
 
     return (
         <View style={styles.container}>
