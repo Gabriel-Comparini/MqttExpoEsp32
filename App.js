2da3a630c4196ff7f9180164d6565a399aabe0a9
Gabriel Comparini <gc77777777777777@gmail.com>
2026-06-12 23:57:10 -0300

    feat: add a graph to list data

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import StatusModal from './src/components/StatusModal';
 import LightControl from './src/components/LightControl';
 import Gauges from './src/components/Gauges';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LineChart } from "react-native-chart-kit";
 import { deleteData, initDatabase, insertNewData, selectData } from './src/services/db';
 
 const mqtt = new MQTTService();
@@ -13,10 +14,13 @@ export default function App() {
     const [isConnected, setIsConnected] = useState(false);
     const [showError, setShowError] = useState(false);
     const [historyVisible, setHistoryVisible] = useState(false);
+    const [dashboardVisible, setDashboardVisible] = useState(false);
     const [isLightOn, setIsLightOn] = useState(false);
     const [temp, setTemp] = useState(0);
     const [hum, setHum] = useState(0);
     const [data, setData] = useState([]);
+    const [humData, setHumData] = useState([]);
+    const [tempData, setTempData] = useState([]);
 
     const mqttConfig = {
         host: process.env.EXPO_PUBLIC_MQTT_HOST,
@@ -58,7 +62,19 @@ export default function App() {
             
             startConnection();
             await initDatabase();
-            setData(await selectData());
+            const rest = await selectData();
+            setData(rest);
+            setTempData(rest
+                .filter(item => item.temp > 0 && item.temp < 100)
+                .map(item => parseFloat(item.temp))
+                // .slice(-15)
+            );
+
+            setHumData(rest
+                .filter(item => item.hum > 0 && item.hum <= 100)
+                .map(item => parseFloat(item.hum))
+                // .slice(-15)
+            );
 
             if (asyncHum !== null) setHum(parseFloat(asyncHum));
             if (asyncTemp !== null) setTemp(parseFloat(asyncTemp));
@@ -74,7 +90,19 @@ export default function App() {
             await AsyncStorage.setItem("temp", String(temp));
             await AsyncStorage.setItem("light", String(isLightOn));
             await insertNewData(hum, temp, isLightOn);
-            setData(await selectData());
+            const rest = await selectData();
+            setData(rest);
+            setTempData(rest
+                .filter(item => item.temp > 0 && item.temp < 100)
+                .map(item => parseFloat(item.temp))
+                // .slice(-50)
+            );
+
+            setHumData(rest
+                .filter(item => item.hum > 0 && item.hum <= 100)
+                .map(item => parseFloat(item.hum))
+                // .slice(-50)
+            );
         }
 
         init();
@@ -82,7 +110,7 @@ export default function App() {
 
     return (
         <View style={styles.screen}>
-            {!historyVisible ? (
+            {!historyVisible && !dashboardVisible ? (
                 <View style={styles.container}>
                     <Text style={styles.header}>Smart Home IoT</Text>
 
@@ -101,8 +129,14 @@ export default function App() {
                             Visualizar histórico
                         </Text>
                     </Pressable>
+
+                    <Pressable style={styles.histBtn} onPress={() => setDashboardVisible(!dashboardVisible)}>
+                        <Text style={styles.btnText}>
+                            Visualizar dashboard
+                        </Text>
+                    </Pressable>
                 </View>
-            ) : (
+            ) : historyVisible ? (
                 <View style={styles.container}>
                     <FlatList 
                         data={data}
@@ -113,7 +147,7 @@ export default function App() {
                                 <Text style={{color: "#F1C40F"}}>
                                     Luz: {item.light ? "ligada" : "desligada"}
                                 </Text>
-                                <Text style={{color: "#3498DB"}}>
+                                <Text style={{color: "#3498db"}}>
                                     Umidade: {item.hum}%
                                 </Text>
                                 <Text style={{color: "#E74C3C"}}>
@@ -127,7 +161,57 @@ export default function App() {
                         }
                     />
 
-                    <Pressable style={styles.histBtn} onPress={() => setHistoryVisible(!historyVisible)}>
+                    <Pressable style={[styles.histBtn, { marginBottom:50 }]} onPress={() => setHistoryVisible(!historyVisible)}>
+                        <Text style={styles.btnText}>
+                            Voltar aos dados
+                        </Text>
+                    </Pressable>
+                </View>
+            ) : (
+                <View style={styles.container}>
+                        <Text style={styles.header}>Variação de Temperatura</Text>
+                        <LineChart 
+                            data={{
+                                datasets: [{ 
+                                    data: tempData
+                                }]
+                            }}
+                            height={200}
+                            width={350}
+                            chartConfig={{
+                                backgroundGradientFrom: '#1a1a1a',
+                                backgroundGradientTo: '#1a1a1a',
+                                color: (opacity = 1) => `rgba(231, 76, 60, ${opacity})`,
+                                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                strokeWidth: 2,
+                            }}
+                            yAxisInterval={10}
+                            withDots={false}
+                            bezier
+                            withYLabels={false}
+                            transparent={true}
+                        />
+                    <Text style={styles.header}>Variação de Umidade</Text>
+                        <LineChart 
+                            data={{
+                                datasets: [{ 
+                                    data: humData
+                                }]
+                            }}
+                            height={200}
+                            width={350}
+                            chartConfig={{
+                                color: (opacity = 1) => `rgb(52, 152, 219, ${opacity})`,
+                                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                strokeWidth: 2,
+                            }}
+                            yAxisInterval={10}
+                            withDots={false}
+                            bezier
+                            withYLabels={false}
+                            transparent={true}
+                        />
+                    <Pressable style={styles.histBtn} onPress={() => setDashboardVisible(!dashboardVisible)}>
                         <Text style={styles.btnText}>
                             Voltar aos dados
                         </Text>
@@ -177,7 +261,6 @@ const styles = StyleSheet.create({
         padding: 17,
         backgroundColor: "#3498DB",
         borderRadius: 15,
-        marginBottom: 50,
         marginTop: 20,
         width: "100%"
     },
